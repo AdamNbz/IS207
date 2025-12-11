5b030ab2b3c057aad6b69749374b03bc98be6c86
HydrogenDrinker <holysheep739@gmail.com>
2025-12-11 13:15:20 +0700

    Update api routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,7 +1,7 @@
 <?php
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\CartController;
-
+use App\Http\Controllers\Api\ProductController;
 use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\BrandController;
 use Illuminate\Support\Facades\Route;
